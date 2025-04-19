ae262e3857d3fb5fbeaf72fcff7c528514ad0bed
YiChun <yichun0701@gmail.com>
2025-04-19 04:58:15 +0000

    Add variables to track the progress

--- include/my_components/attach_client_component.hpp
+++ include/my_components/attach_client_component.hpp
@@ -3,6 +3,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "attach_shelf/srv/go_to_loading.hpp"
+#include "std_msgs/msg/string.hpp"
 
 namespace my_components {
 
@@ -14,17 +15,32 @@ private:
     // Service client
     rclcpp::Client<attach_shelf::srv::GoToLoading>::SharedPtr client_;
     
+    // Subscription to detect lift commands
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr lift_subscription_;
+    
     // Timer to check service availability and call it
     rclcpp::TimerBase::SharedPtr timer_;
     
     // Flag to track if service has been called
     bool service_called_;
     
+    // Flag to track if service has completed
+    bool service_completed_;
+    
+    // Flag to track if we're waiting for response
+    bool waiting_for_response_;
+    
+    // Counter for waiting time
+    int wait_count_;
+    
     // Shutdown counter
     int shutdown_counter_;
     
     // Timer callback
     void timer_callback();
+    
+    // Lift topic callback
+    void lift_callback(const std_msgs::msg::String::SharedPtr msg);
 };
 
 } // namespace my_components
